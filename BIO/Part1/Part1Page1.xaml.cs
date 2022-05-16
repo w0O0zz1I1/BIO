a6d2a8478135613f87dee16d3640db4e4bfc7051
w0zz1 <d77cf2f4>
2022-05-16 15:28:01 +0300

    Добавьте файлы проекта.

---
+++ BIO/Part1/Part1Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BIO.Part1
+{
+    /// <summary>
+    /// Логика взаимодействия для Part1Page1.xaml
+    /// </summary>
+    public partial class Part1Page1 : Page
+    {
+        public Part1Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = Window.GetWindow(this) as MainMenu;
+            mainMenu.mainFrame.Navigate(new MainPage());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = Window.GetWindow(this) as MainMenu;
+            mainMenu.mainFrame.Navigate(new Part1.Part1Page2());
+        }
+    }
+}
---
+++ BIO/Part1/Part1Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BIO.Part1
+{
+    /// <summary>
+    /// Логика взаимодействия для Part1Page2.xaml
+    /// </summary>
+    public partial class Part1Page2 : Page
+    {
+        public Part1Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
