896df8dc9597cea5cba5be26a5b8292e31cf20f7
w0zz1 <d77cf2f4>
2022-06-06 19:18:03 +0300

    add some part

---
+++ BIO/Part2/Part2Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BIO.Part2
+{
+    /// <summary>
+    /// Логика взаимодействия для Part2Page2.xaml
+    /// </summary>
+    public partial class Part2Page2 : Page
+    {
+        public Part2Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BIO/Part3/Part3Page3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BIO.Part3
+{
+    /// <summary>
+    /// Логика взаимодействия для Part3Page3.xaml
+    /// </summary>
+    public partial class Part3Page3 : Page
+    {
+        public Part3Page3()
+        {
+            InitializeComponent();
+        }
+    }
+}
