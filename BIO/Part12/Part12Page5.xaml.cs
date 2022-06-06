ac2a5a6a05a86af324f474a44dcf90624d807f37
w0zz1 <d77cf2f4>
2022-06-06 20:47:35 +0300

    add part 12

---
+++ BIO/Part12/Part12Page5.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BIO.Part12
+{
+    /// <summary>
+    /// Логика взаимодействия для Part12Page5.xaml
+    /// </summary>
+    public partial class Part12Page5 : Page
+    {
+        public Part12Page5()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BIO/Part12/Part12Page6.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BIO.Part12
+{
+    /// <summary>
+    /// Логика взаимодействия для Part12Page6.xaml
+    /// </summary>
+    public partial class Part12Page6 : Page
+    {
+        public Part12Page6()
+        {
+            InitializeComponent();
+        }
+
+        private void answer1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void answer4_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void answer5_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
